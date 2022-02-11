1cc8230bdfb88746b0b9f9c0ec3b45a8f7421bc0
bananavoid <lytvyn.wokmail@gmail.com>
2022-02-11 22:08:28 +0100

    add coil for image loading

--- app/src/main/java/com/lovely/weatherstation/ui/CityWeatherItem.kt
+++ app/src/main/java/com/lovely/weatherstation/ui/CityWeatherItem.kt
@@ -17,8 +17,8 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.layout.ContentScale
-import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
+import coil.compose.rememberImagePainter
 
 class CityWeatherItem(
     val viewModel: CityWeatherViewModel
@@ -35,7 +35,7 @@ class CityWeatherItem(
         ) {
             Row {
                 Image(
-                    painter = painterResource(id = viewModel.weatherIcon.value),
+                    painter = rememberImagePainter(viewModel.weatherIcon),
                     contentDescription = null,
                     contentScale = ContentScale.Crop,
                     modifier = Modifier
